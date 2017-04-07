c5f62ce13304ea0821281bbef1297835c1424749
Gonzalo <gonzalocasal@gmail.com>
2017-04-07 10:47:31 -0300

    geters y seters datos usuario

--- src/main/java/ar/com/gruponn/model/Usuario.java
+++ src/main/java/ar/com/gruponn/model/Usuario.java
@@ -6,6 +6,47 @@ import java.util.List;
 public abstract class Usuario {
 	
 	protected List<Viaje> viajes;
+	protected String usuario;
+	protected String contraseña;
+	protected String nombre;
+	protected String apellido;
+	
+	public List<Viaje> getViajes() {
+		return viajes;
+	}
+	public void setViajes(List<Viaje> viajes) {
+		this.viajes = viajes;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
 	protected Date fechaAlta;
 	 
 
