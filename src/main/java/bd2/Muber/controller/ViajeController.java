55ac0a805c6ee090d0276ba02b9df1d23fe9cb0e
Gonzalo Casal <gonzalocasa@gmail.com>
2017-05-07 07:39:53 -0300

    put agregar pasajero

--- src/main/java/bd2/Muber/controller/ViajeController.java
+++ src/main/java/bd2/Muber/controller/ViajeController.java
@@ -11,10 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import bd2.Muber.dto.PasajeroDTO;
 import bd2.Muber.dto.ViajeDTO;
 import bd2.Muber.model.Conductor;
+import bd2.Muber.model.Pasajero;
 import bd2.Muber.model.Viaje;
 import bd2.Muber.service.ConductorService;
+import bd2.Muber.service.PasajeroService;
 import bd2.Muber.service.ViajeService;
 
 @ControllerAdvice
@@ -25,6 +28,9 @@ public class ViajeController {
 
 	@Autowired
 	private ConductorService conductorService;
+	
+	@Autowired
+	private PasajeroService pasajeroService;
 	
 	@Autowired
 	private ViajeService service;
@@ -51,6 +57,20 @@ public class ViajeController {
 		aMap.put("Viajes Creado", new ViajeDTO(nuevoViaje));
 		return aMap;
 	}
+	
+	
+	@RequestMapping(value = "/viajes/agregarPasajero", method = RequestMethod.PUT, produces = "application/json", headers = "Accept=application/json")
+	public Map<String, Object> agregarPasajero( @RequestParam("viajeId") int viajeId,
+											 @RequestParam("pasajeroId") int pasajeroId) {
+		Viaje viaje = service.obtenerViaje(viajeId);
+		Pasajero pasajero = pasajeroService.obtenerPasajero(pasajeroId);
+		service.registrarPasajero(viaje, pasajero);
+		Map<String, Object> aMap = new HashMap<String, Object>();
+		aMap.put("result", "OK");
+		aMap.put("Pasajero Agregado", new PasajeroDTO(pasajeroService.obtenerPasajero(pasajeroId)));
+		return aMap;
+	}
+	
 }
 
 
